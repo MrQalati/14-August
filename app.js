d3acc5a18d340b344039e810fe13cb2fabdb5b2b
MrQalati <hashkan90@gmail.com>
2023-08-15 15:50:41 +0100

    fixed task 2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const {getAllTopics} = require ('./controllers/topics-controller')
 
-// app.use(express.json())
 
 app.get('/api/topics', getAllTopics)
 
--- models/topics-models.js
+++ models/topics-models.js
@@ -7,12 +7,7 @@ const db = require ('../db/connection')
 exports.fetchAllTopics = () => {
 
     return db.query('SELECT * FROM topics;').then((result) => {
-        const topics = result.rows;
-
-        if (topics.length === 0) {
-            throw { status: 404, msg: 'No topics found' }
-            }      
-               
-            return topics
+    const topics = result.rows;
+        return topics
     })
 }
