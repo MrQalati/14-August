45429a835af125c92e700ffb498e0170f1e40895
MrQalati <hashkan90@gmail.com>
2023-08-18 09:12:25 +0100

    new edit on task5

--- controllers/new-articles-controllers.js
+++ controllers/new-articles-controllers.js
@@ -1,16 +1,9 @@
 const { fetchAllArticles } = require('../models/new-articles-models');
 
 exports.getAllArticles = (req, res, next) => {
-    fetchAllArticles()
-      .then((articles) => {
-        const modifiedArticles = articles.map((article) => {
-          const { body, ...rest } = article;
-          return {
-            ...rest,         
-            comments: article.comments,
-          };
-        });
-        res.status(200).send({ articles: modifiedArticles });
-      })
-      .catch(next);
-  };
+  fetchAllArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
+    })
+    .catch(next);
+};
